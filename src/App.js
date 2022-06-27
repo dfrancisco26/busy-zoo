593a6d7421be27df4deca2c3e08ccb433772c5f7
David Francisco <dfrancisco2677@gmail.com>
2022-06-26 20:05:44 -0700

    Fixed test errors.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import OpenClosedSign from './OpenClosedSign.js';
+import OpenClosedSign from './OpenClosedSign';
 import './App.css';
-import AnimalList from './AnimalList.js';
+import AnimalList from './AnimalList';
 function App() {
 
 
